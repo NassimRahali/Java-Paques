c8123438088d8f3b975d8c34a379efc133d64509
KaouAha <kaou-aha-3@hotmail.com>
2015-04-27 01:26:36 +0200

    script sql + keystore

---
+++ MySQL/requetes.sql
@@ -0,0 +1,45 @@
+
+---------------
+Requête 1 (OK)
+---------------
+select * from operations where ClientID = (select IdClient from clients where Nom='Client1')
+	and  DateOp between '2015-01-02' and '2015-01-08';
+-----------------------------------------------------------------------------------------------------------------
+
+
+
+---------------
+Requête 2
+---------------
+select avg(Montant) from comptes where ClientID = (select IdClient from clients where Nom='Client1');
+-----------------------------------------------------------------------------------------------------------------
+
+
+
+---------------
+Requête 3
+---------------
+select count(o.TypeOp) as nbre, year(o.DateOp) from operations o,comptes c
+	where o.Traitee = 0 and o.TypeOp = "débit"
+	and o.ClientID = (select IdClient from clients where Nom='Client1')
+	and c.NumCompte in (select NumCompte from comptes where ClientID = (select IdClient from clients where Nom='Client1'))
+	group by year(o.DateOp);
+-----------------------------------------------------------------------------------------------------------------
+
+
+
+---------------
+Requête 4
+---------------
+select Fiable as Validation from comptes where Fiable = 1 and IdCompte = 1
+	and ClientID = (select IdClient from clients where Nom='Client1');
+-----------------------------------------------------------------------------------------------------------------
+
+
+
+
+select * from comptes where ClientID = 1;
+
+select * from operations o where ClientID = (select IdClient from clients where Nom='Client1');
+
+select count(*) from operations o, comptes c;
